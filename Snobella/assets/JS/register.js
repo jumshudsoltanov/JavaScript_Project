96f3f58703b222eb24d9a036e1cc039e319eeb1b
JumshudSoltanov <soltanovc56@gmail.com>
2025-04-19 23:17:37 +0400

    created register.js

--- Snobella/assets/JS/register.js
+++ Snobella/assets/JS/register.js
@@ -1,22 +1,42 @@
-// Put input data 
+// Put input data
 
 const fullname = document.getElementById("fullname");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const sendBtn = document.getElementById("sendBtn");       
+const sendBtn = document.getElementById("sendBtn");
+
+sendBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  // Form Value
+  fullnameValue = fullname.value;
+  usernameValue = username.value;
+  emailValue = email.value;
+  passwordValue = password.value;
+
+
+    if (fullnameValue === "" && usernameValue === "" && emailValue === "" && passwordValue === "") {
+      alert(" Bütün Məlumatları daxil edin!");
+    }
+    else {
+
+        const response = await fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                fullname: fullnameValue,
+                username: usernameValue,
+                email: emailValue,
+                password: passwordValue,
+            }),
+        })
+    }
 
 
 
-sendBtn.addEventListener("click",  (e) => {
 
 
-
-
-
-
-
-
-
-    
-})
+});
