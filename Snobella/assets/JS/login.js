781801cf8509182e4aaf2b408337c6283aba7a7f
JumshudSoltanov <soltanovc56@gmail.com>
2025-04-20 00:01:19 +0400

    login and register end

--- Snobella/assets/JS/login.js
+++ Snobella/assets/JS/login.js
@@ -1,2 +1,22 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const sendBtn = document.getElementById("sendBtn");
+
+sendBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const emailValue = email.value;
+  const passwordValue = password.value;
+
+  const dbData = await fetch("http://localhost:3000/users");
+  const users = await dbData.json();
+
+  const user = users.find(
+    (user) => user.email === emailValue && user.password === passwordValue
+  );
+  if (user) {
+    alert("Giris Uğurlu!");
+    window.location.href = "home.html";
+  } else {
+    alert("Parol veya email yanlis!");
+  }
+});
