04a2eaf7dceb517b482c5af142eb442988bef4d3
JumshudSoltanov <soltanovc56@gmail.com>
2025-04-19 23:10:16 +0400

    created db.json

---
+++ Snobella/assets/JS/register.js
@@ -0,0 +1,22 @@
+// Put input data 
+
+const fullname = document.getElementById("fullname");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const sendBtn = document.getElementById("sendBtn");       
+
+
+
+sendBtn.addEventListener("click",  (e) => {
+
+
+
+
+
+
+
+
+
+    
+})
